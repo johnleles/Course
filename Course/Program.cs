6fa8b63ca6316f6e00069e60e1c91e36f0629528
João Wictor Leles <johnleles@outlook.com>
2020-04-21 16:44:07 -0300

    adicionando projeto

---
+++ Course/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, horasTrabalhadas;
+            double valorPorHora, salarioFinal;
+
+            //entrada
+
+            X = int.Parse(Console.ReadLine());
+            horasTrabalhadas = int.Parse(Console.ReadLine());
+            valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //processamento
+
+            salarioFinal = valorPorHora * horasTrabalhadas;
+ 
+
+            //saída
+
+            Console.WriteLine("NUMBER = " + X);
+            Console.WriteLine("SALARY = U$ " + salarioFinal.ToString("F2", CultureInfo.InvariantCulture));
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
